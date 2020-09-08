f5d5c434b8d6edd8b18568d0566840e86965b84b
Yuri Nosov <48147692+zashib@users.noreply.github.com>
2020-09-08 10:33:59 +0200

    Add router

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/zashib/gin_server
 go 1.15
 
 require (
+	github.com/gin-gonic/gin v1.6.3
 	github.com/lib/pq v1.8.0
 	xorm.io/xorm v1.0.4
 )
--- main.go
+++ main.go
@@ -1,81 +1,16 @@
 package main
 
 import (
-	"fmt"
-	_ "github.com/lib/pq"
-	"time"
-	"xorm.io/xorm"
+	"github.com/gin-gonic/gin"
 )
 
-type Note struct {
-	Id      int64
-	Name    string `xorm:"notnull unique"`
-	Content string
-	Status  time.Time
-}
-
-func connectionString() string {
-	return fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", "localhost", "5434", "user", "example", "pwd")
-}
-
-func dataInsert(note Note, engine *xorm.Engine) {
-	note.Status = time.Now()
-	_, err := engine.InsertOne(&note)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-func dataGetByName(name string, engine *xorm.Engine) interface{} {
-	var note = Note{
-		Name: name,
-	}
-	_, err := engine.Get(&note)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return note
-}
-
-func dataUpdateContentByName(name string, newContent string, engine *xorm.Engine) {
-	_, err := engine.In("name", name).Update(&Note{Content: newContent, Status: time.Now()})
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-func dataDeleteByName(name string, engine *xorm.Engine){
-	_, err := engine.Delete(Note{Name: name})
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-// TODO autoupdate time func when data changed
-// TODO set unique not null(default) values for cols
 func main() {
-	engine, err := xorm.NewEngine("postgres", connectionString())
-	if err != nil {
-		fmt.Println(err)
-	}
-	err = engine.Sync2(new(Note))
-	if err != nil {
-		fmt.Println(err)
-	}
+	router := gin.Default()
 
-	note := Note{
-		Name:    "test",
-		Content: "some contetnt",
-	}
+	router.POST("/note")
+	router.GET("/note/:name")
+	router.PUT("/note/:name")
+	router.DELETE("/note/:name")
 
-	dataInsert(note, engine)
-
-	name := "test1"
-	fmt.Println(dataGetByName(name, engine))
-
-	dataUpdateContentByName(name, "else", engine)
-
-	name = "test2"
-	dataDeleteByName(name, engine)
-
+	router.Run()
 }
