8a8f1ce568422b97f7af71b27772903b4a003365
Yuri Nosov <nosov@buroburo.ru>
2020-09-11 10:30:39 +0200

    Add json handler to POST method

--- main.go
+++ main.go
@@ -7,7 +7,16 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.POST("/note")
+	router.POST("/note", func(c *gin.Context) {
+		body := struct {
+			Name string
+		}{}
+		c.BindJSON(&body)
+		c.JSON(200, gin.H{
+			"message": "Hello World",
+			"name":    body.Name,
+		})
+	})
 	router.GET("/note/:name")
 	router.PUT("/note/:name")
 	router.DELETE("/note/:name")
