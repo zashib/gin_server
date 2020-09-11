3390724b59c8d04ec03eec945cb9949ef90954e9
Yuri Nosov <nosov@buroburo.ru>
2020-09-11 10:29:16 +0200

    Add database structure

--- db.go
+++ db.go
@@ -14,44 +14,15 @@ type Note struct {
 	Status  time.Time
 }
 
+type Database struct {
+	engine xorm.Engine
+}
+
 func connectionString() string {
 	return fmt.Sprintf("host=%s port=%s user=%s dbname=%s password=%s sslmode=disable", "localhost", "5434", "user", "example", "pwd")
 }
 
-func dataInsert(note Note, engine *xorm.Engine) {
-	note.Status = time.Now()
-	_, err := engine.InsertOne(&note)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-func dataGetByName(name string, engine *xorm.Engine) interface{} {
-	var note = Note{
-		Name: name,
-	}
-	_, err := engine.Get(&note)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return note
-}
-
-func dataUpdateContentByName(name string, newContent string, engine *xorm.Engine) {
-	_, err := engine.In("name", name).Update(&Note{Content: newContent, Status: time.Now()})
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-func dataDeleteByName(name string, engine *xorm.Engine) {
-	_, err := engine.Delete(Note{Name: name})
-	if err != nil {
-		fmt.Println(err)
-	}
-}
-
-func createDB() *xorm.Engine {
+func (db Database) init() Database {
 	engine, err := xorm.NewEngine("postgres", connectionString())
 	if err != nil {
 		fmt.Println(err)
@@ -60,27 +31,60 @@ func createDB() *xorm.Engine {
 	if err != nil {
 		fmt.Println(err)
 	}
-	return engine
+	db.engine = *engine
+	return db
 }
 
-// TODO autoupdate time func when data changed
+func (db Database) dataInsert(note Note) {
+	note.Status = time.Now()
+	_, err := db.engine.InsertOne(&note)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func (db Database) dataGetByName(name string) interface{} {
+	var note = Note{
+		Name: name,
+	}
+	_, err := db.engine.Get(&note)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return note
+}
+
+func (db Database) dataUpdateContentByName(name string, newContent string) {
+	_, err := db.engine.In("name", name).Update(&Note{Content: newContent, Status: time.Now()})
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func (db Database) dataDeleteByName(name string) {
+	_, err := db.engine.Delete(Note{Name: name})
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
 // TODO set unique not null(default) values for cols
 func main() {
-	engine := createDB()
+	db := Database{}.init()
 
 	note := Note{
 		Name:    "test",
 		Content: "some contetnt",
 	}
 
-	dataInsert(note, engine)
+	db.dataInsert(note)
 
 	name := "test1"
-	fmt.Println(dataGetByName(name, engine))
+	fmt.Println(db.dataGetByName(name))
 
-	dataUpdateContentByName(name, "else", engine)
+	db.dataUpdateContentByName(name, "else")
 
 	name = "test2"
-	dataDeleteByName(name, engine)
+	db.dataDeleteByName(name)
 
 }
